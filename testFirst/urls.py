166a31e983f18fb34501be768e3c29b4134bfc0e
fuqiang89 <513914894@qq.com>
2013-11-28 16:54:17 +0800

    tongbu
    Signed-off-by: fuqiang89 <513914894@qq.com>

--- testFirst/urls.py
+++ testFirst/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from app.te import current_datetime
+from django.conf import settings
+import os
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,5 +10,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'testFirst.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^f/$',current_datetime),
-)
+    url(r'^f/$',current_datetime),)
+
+
